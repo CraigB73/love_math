240bde0943edae1e6f09dbd6244e69de38532cc6
CraigB73 <craigbritton73@gmail.com>
2023-11-04 19:55:18 +0100

    Add random number to runGame and function that handles addition as default when game loads

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Wait for the Dom to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName('button');
+
+  for(let button of buttons){
+    button.addEventListener('click', function() {
+      if ( this.getAttribute('data-type') === 'submit'){
+        alert('You clicked submit');
+      }else {
+        let gameType =  this.getAttribute('data-type');
+        runGame(gameType);
+      }
+    })
+  }
+  runGame('addition');
+})
+
+/**
+ * The main game "loop", called when the script is first load
+ * and after the user's answer has been processed
+*/
+function runGame(gameType) {
+  // Creates two random number between 1 - 25
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === 'addition'){
+    displayAdditionQuestion(num1, num2); 
+  }else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
+}
+function checkAnswer() {}
+function calculateCorrectAnswer() {}
+function incrementScore() {}
+function incrementWrongAnswer() {}
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '+';
+
+}
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '-';
+}
+function displayMultiplyQuestion(operand1, operand2) {
+   document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '*';
+}
