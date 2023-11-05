bd99c324ae6fc4893a5cdb104a6bc399a1253dab
CraigB73 <craigbritton73@gmail.com>
2023-11-05 17:03:50 +0100

    Code clean up added division function and eventlistener to the input for keypress: Enter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // Wait for the Dom to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function() {
   let buttons = document.getElementsByTagName('button');
-
   for(let button of buttons){
     button.addEventListener('click', function() {
       if ( this.getAttribute('data-type') === 'submit'){
@@ -12,6 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     })
   }
+  document.getElementById('answer-box').addEventListener('keydown', (event) => {
+    if(event.key === 'Enter'){
+      checkAnswer()
+    }
+
+  })
   runGame('addition');
 })
 
@@ -20,16 +25,31 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
 */
 function runGame(gameType) {
+  // Clear input box
+  document.getElementById('answer-box').value = '';
+  document.getElementById('answer-box').focus();
+
   // Creates two random number between 1 - 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
-
+  
   if (gameType === 'addition'){
-    displayAdditionQuestion(num1, num2); 
-  }else {
+    displayAdditionQuestion(num1, num2);  
+    console.log(num1 + num2)
+  }else if (gameType === 'subtract') {
+    displaySubtractQuestion(num1, num2)
+    console.log(num1 - num2)
+  }else if (gameType === 'multiply'){
+    displayMultiplyQuestion(num1, num2)
+    console.log(num1 * num2)
+  }else if (gameType === 'division'){
+    displayDivisionQuestion(num1, num2)
+   console.log(num1 /num2)
+  }else{
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
   }
+
 }
 
 /**
@@ -44,9 +64,11 @@ function checkAnswer() {
   if(isCorrect){
     alert('Hey you got it right! :D');
     incrementScore()
+    
   }else {
     alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}!`)
     incrementWrongAnswer()
+    document.getElementById('answer-box').value = ''
   }
   runGame(calculateAnswer[1]);
 }
@@ -62,7 +84,13 @@ function calculateCorrectAnswer() {
 
   if(operator === '+'){
     return [operand1 + operand2, 'addition'];
-  }else {
+  }else if(operator === 'x'){
+    return [operand1 * operand2, 'multiply'];
+  }else if(operator === '-'){
+    return [operand1 - operand2, 'subtract'];
+  }else if(operator === '/'){
+      return [operand1 / operand2, 'division'];
+    } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
   }
@@ -97,12 +125,19 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 function displaySubtractQuestion(operand1, operand2) {
-  document.getElementById('operand1').textContent = operand1;
-  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
   document.getElementById('operator').textContent = '-';
 }
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
-  document.getElementById('operand2').textContent = operand2;
-  document.getElementById('operator').textContent = '*';
+  document.getElementById('operand2').textContent = operand1;
+  document.getElementById('operator').textContent = 'x';
+
+}
+function displayDivisionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand2 > operand1 ? operand2 : operand1;
+  document.getElementById('operator').textContent = '/';
+ 
 }
