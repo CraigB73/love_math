787fe12725260321169866f93958178e05c91b3c
CraigB73 <craigbritton73@gmail.com>
2023-11-07 18:33:31 +0100

    Add division question to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ function runGame(gameType) {
     console.log(num1 * num2)
   }else if (gameType === 'division'){
     displayDivisionQuestion(num1, num2)
-   console.log(num1 /num2)
+   console.log(num1 , num2,)
   }else{
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -101,9 +101,7 @@ function calculateCorrectAnswer() {
  */
 function incrementScore() {
   let oldScore  = parseInt(document.getElementById('score').innerText);
-  document.getElementById('score').innerText = ++oldScore;
- 
-    
+  document.getElementById('score').innerText = ++oldScore;  
 }
 
 /**
@@ -124,20 +122,29 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById('operator').textContent = '+';
 
 }
+/**
+ * Displays substaction and check so not to return a negative number, math questions
+ */
 function displaySubtractQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
   document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
   document.getElementById('operator').textContent = '-';
 }
+/**
+ * Displays mulitplication math questions
+ */
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
-  document.getElementById('operand2').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = 'x';
 
 }
+/**
+ * Displays division and insures not to return a remainder math questions
+ */
 function displayDivisionQuestion(operand1, operand2) {
-  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-  document.getElementById('operand2').textContent = operand2 > operand1 ? operand2 : operand1;
-  document.getElementById('operator').textContent = '/';
- 
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand1;
+    document.getElementById('operator').textContent = '/';
+
 }
